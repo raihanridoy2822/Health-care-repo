46b1d9c35d0c0215347930fb76822338e0241bc9
raihanridoy2822 <raihanridoy1894@gmail.com>
2022-09-29 17:00:00 +0600

    border button

--- src/Fitness/Fitness.js
+++ src/Fitness/Fitness.js
@@ -3,10 +3,18 @@ import './Fitness.css'
 const Fitness = (props) => {
     const { name, img, age, time } = props.product;
     return (
-        <div className='fitness-img'>
-            <img src={img} alt=""></img>
-            <p>{name}</p>
+        <div className='total-fitness'>
+            <div className='fitness-img'>
+                <img src={img} alt=""></img>
+                <h3>{name}</h3>
+                <p>age:{age}</p>
+                <p>time requirement: {time}</p>
+            </div>
+            <button className='btn-card'>
+                <p>Add to list</p>
+            </button>
         </div>
+
     );
 };
 
